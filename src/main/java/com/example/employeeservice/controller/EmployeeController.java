2850dafd01551651198ea1b1974884671a76eaa0
Victor Felipe Jr <23023987@saultcollege.ca>
2024-06-16 09:57:44 -0400

    added CORS configuration to work around browser blocking same-origin policy issue

--- src/main/java/com/example/employeeservice/controller/EmployeeController.java
+++ src/main/java/com/example/employeeservice/controller/EmployeeController.java
@@ -4,6 +4,7 @@ package com.example.employeeservice.controller;
 import com.example.employeeservice.model.Employee;
 import com.example.employeeservice.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,11 +13,15 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/coding/test")
+@CrossOrigin(origins = "http://localhost:4200")
 public class EmployeeController {
 
     @Autowired
     private EmployeeService employeeService;
 
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
     @GetMapping("/employees")
     public List<Employee> getEmployeesWithMoreThan10Years() {
         return employeeService.getEmployeesWithMoreThan10Years();
